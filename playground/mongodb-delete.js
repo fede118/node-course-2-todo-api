0aa091b964eba3882a1fe27e5d72a7d7485d5be2
Fede Cano <fede.cano118@gmail.com>
2019-01-09 15:13:55 -0300

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true}, (err, client) => {
+	if (err) {
+		return console.log('Unable to connect to mongodb.')
+	}
+	console.log('Connected to mongodb');
+	const db = client.db('TodoApp');
+
+	//deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// findOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// db.collection('Users').deleteMany({user: 'Fede'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	var id = new ObjectID('5c34ac3d977d05149caea8aa')
+	db.collection('Users').findOneAndDelete({_id: id}).then((result) => {
+		console.log('result');
+	});
+
+	// client.close();
+});
