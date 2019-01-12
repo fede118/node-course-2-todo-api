804e3113bf4fc1896ad7c7e88d2bf01330adc715
Fede Cano <fede.cano118@gmail.com>
2019-01-12 10:57:16 -0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -26,7 +27,24 @@ app.get('/todos', (req, res) => {
 		res.send({todos});
 	}, (e) => {
 		res.status(400).send(e);
-	})
+	});
+});
+
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((doc) => {
+		if (!doc) {
+			return res.status(404).send()
+		}
+
+		res.send({doc});
+	}).catch((e) => {
+		res.status(400).send();
+	});
 });
 
 app.listen(3000, () => {
