87ce308456ab2328c15f2d237859d411b6129b55
Fede Cano <fede.cano118@gmail.com>
2019-01-13 12:22:54 -0300

    Still fixing mongodb local or heroku priorities

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
-
+let db = {
+	localhost: 'mongodb://localhost:27017/TodoApp',
+	mlab: "mongodb://fede118:lacuenta1@ds255784.mlab.com:55784/todo-api"
+}
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true} || "mongodb://fede118:lacuenta1@ds255784.mlab.com:55784/todo-api");
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost, {useNewUrlParser: true});
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true} || "mongodb://fede118:lacuenta1@ds255784.mlab.com:55784/todo-api");
 
 module.exports = {mongoose};
