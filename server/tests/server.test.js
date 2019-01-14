ff9d376d34d9cce88c406f61cdc26f67d0cbaf93
Fede Cano <fede.cano118@gmail.com>
2019-01-14 12:58:21 -0300

    Add delete test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,41 @@ describe('GET /todos/:id', () => {
 			.end(done);
 	});
 });
+
+describe("DELETE /todos/:id", () => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[1]._id.toHexString();
+
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+
+				Todo.findById(hexId).then((doc) => {
+					expect(doc).toBeNull();
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		var id = new ObjectID().toHexString();
+		request(app)
+			.delete(`/todos/${id}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if object id is invalid', (done) => {
+		request(app)
+			.get('/todos/123456')
+			.expect(404)
+			.end(done);
+	});
+});
