078b825a0e878b47370396ae99945da94a728bb0
Fede Cano <fede.cano118@gmail.com>
2019-01-14 15:37:55 -0300

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-let db = {
-	localhost: 'mongodb://localhost:27017/TodoApp',
-	mlab: "mongodb://fede118:lacuenta1@ds255784.mlab.com:55784/todo-api"
-}
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost, {useNewUrlParser: true});
-// mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true} || "mongodb://fede118:lacuenta1@ds255784.mlab.com:55784/todo-api");
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = {mongoose};
