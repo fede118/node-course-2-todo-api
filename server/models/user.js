de7118516718dc877d372799a6a2e75b754a741b
Fede Cano <fede.cano118@gmail.com>
2019-01-15 18:49:46 -0300

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,14 @@ UserSchema.methods.generateAuthToken = function () {
 	return user.save().then(() => token);
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {tokens: {token}}
+	})
+};
+
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
 	var decoded;
